2e099d6f847197072f52cd3c5a7106ac51a16b67
BPowell2 <112312288+BPowell2@users.noreply.github.com>
2022-09-28 21:08:13 -0400

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,15 @@
+ /*9/25/22 this is a application for our stacks for palidromes checker
+ * @author Benjamin Powell
+ */
+import java.util.*;
+import java.io.*;
+
+public class Application {
+
+	public static void main(String[]args) {
+		Palindrome Entry1 = new Palindrome();
+		System.out.println(Entry1.isPalindrome("Hello"));
+		System.out.println(Entry1.isPalindrome("racecar"));
+	}//end of main
+	
+}//end of class
